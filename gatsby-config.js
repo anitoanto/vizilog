dbd7350b021fb4c252fcbadaea6909f575f473ec
Anito Anto <anitoperamangalam@gmail.com>
2021-05-19 14:25:51 +0530

    Connected with Firebase

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,20 @@ module.exports = {
   siteMetadata: {
     title: "Vizilog",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "AIzaSyCh0pwkGXfykIzZD9Zb1K1lBmy71IPY4Ec",
+          authDomain: "vizlog-9200d.firebaseapp.com",
+          projectId: "vizlog-9200d",
+          storageBucket: "vizlog-9200d.appspot.com",
+          messagingSenderId: "270071839064",
+          appId: "1:270071839064:web:00e0423ab7f2819da4cd2d",
+          measurementId: "G-2JL5YGKYW6",
+        },
+      },
+    },
+  ],
 };
