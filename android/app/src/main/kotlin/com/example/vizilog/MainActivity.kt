6598c641f1d5029e8eda6fc62ae942b2990fddd2
Anito Anto <anitoperamangalam@gmail.com>
2021-05-19 13:45:22 +0530

    Initial React Gatsby Site

--- android/app/src/main/kotlin/com/example/vizilog/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vizilog
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/wrapper.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:vizilog/pages/home.dart';
-
-class Wrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Home();
-  }
-}
