855091d7f6055da12b32edf1a8d3cd4d7e6a98e9
Your Name <iamabrarsaifudheen@gmail.com>
2021-05-21 01:00:05 +0530

    Authentication Finished,Error Handling Remaining

---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+import 'package:vizilog/pages/home.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => SafeArea(
+        child: IntroductionScreen(
+          pages: [
+            PageViewModel(
+              title: 'Join',
+              body: 'Join us by creating an account via google ',
+              image: buildImage('assets/images/join.png'),
+              decoration: getPageDecoration(),
+            ),
+            PageViewModel(
+              title: 'Scan ',
+              body: 'Scan the QR provided at the shop',
+              image: buildImage('assets/images/scan.png'),
+              decoration: getPageDecoration(),
+            ),
+            PageViewModel(
+              title: 'Track',
+              body: 'Enjoy the effortless tracking experience ',
+              image: buildImage('assets/images/track.png'),
+              decoration: getPageDecoration(),
+            ),
+            
+          ],
+          done: Text('Take me in', style: TextStyle(fontWeight: FontWeight.w600)),
+          onDone: () => goToHome(context),
+          showSkipButton: true,
+          skip: Text('Skip'),
+          onSkip: () => goToHome(context),
+          next: Icon(Icons.arrow_forward),
+          dotsDecorator: getDotDecoration(),
+          onChange: (index) => print('Page $index selected'),
+          globalBackgroundColor: Theme.of(context).primaryColor,
+          skipFlex: 0,
+          nextFlex: 0,
+          // isProgressTap: false,
+          // isProgress: false,
+          // showNextButton: false,
+          // freeze: true,
+          // animationDuration: 1000,
+        ),
+      );
+
+  void goToHome(context) => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => Home()));
+      
+
+  Widget buildImage(String path) =>
+      Center(child: Image.asset(path, width: 350));
+
+  DotsDecorator getDotDecoration() => DotsDecorator(
+        color: Color(0xFFBDBDBD),
+        //activeColor: Colors.orange,
+        size: Size(10, 10),
+        activeSize: Size(22, 10),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+      );
+
+  PageDecoration getPageDecoration() => PageDecoration(
+        titleTextStyle: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+        bodyTextStyle: TextStyle(fontSize: 20),
+        descriptionPadding: EdgeInsets.all(16).copyWith(bottom: 0),
+        imagePadding: EdgeInsets.all(24),
+        pageColor: Colors.white,
+      );
+}
