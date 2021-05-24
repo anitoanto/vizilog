f5aeb3752ed61ef028300dffb33968ff7c6e7362
Deepak M S <souparnikad40@gmail.com>
2021-05-24 21:03:23 +0530

    sidebar and update details file addded

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -7,6 +7,7 @@ import 'package:vizilog/pages/models/user_details.dart';
 import 'package:vizilog/pages/widgets/custom_button.dart';
 import 'package:vizilog/service/auth.dart';
 import 'package:vizilog/service/database.dart';
+import 'package:vizilog/sidebar/update_details.dart';
 import '../prefs.dart';
 import './recent_activities_list.dart';
 import '../models/shop_entries.dart';
@@ -87,6 +88,76 @@ class _HomeState extends State<Home> {
         value: DatabaseService().entries,
         builder: (context, snapshot) {
           return Scaffold(
+            appBar: AppBar(
+              elevation: 0,
+            ),
+            drawer: Drawer(
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.all(20),
+                    color: Theme.of(context).primaryColor,
+                    child: Center(
+                      child: Column(
+                        children: [
+                          Container(
+                            width: 100,
+                            height: 100,
+                            margin: EdgeInsets.only(top: 40, bottom: 10),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                image: NetworkImage(
+                                    'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.Nuy39yqcMREaqhbbevS-YgHaHa%26pid%3DApi&f=1'),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            '$name',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.update),
+                    title: Text(
+                      'Update Details',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (builder) => UpdateDetails()));
+                    },
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.logout),
+                    title: Text(
+                      "Log Out",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    onTap: () async {
+                      await _authService.signOut();
+                    },
+                  ),
+                ],
+              ),
+            ),
             body: SingleChildScrollView(
               child: SafeArea(
                 child: Container(
